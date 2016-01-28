07cb05bda987f79ec04495a8108449afa05f3b44
Anders Sildnes <andsild@gmail.com>
2016-01-28 16:15:10 +0100

    Friends, Romans, Hipsters, Let me clue you in; I come to put down Caesar, not to groove him. The square kicks some cats are on stay with them; The hip bits, like, go down under;  so let it lay with Caesar.  The cool Brutus Gave you the message: Caesar had big eyes; If that's the sound, someone's copping a plea, And, like, old Caesar really set them straight. Here, copacetic with Brutus and the studs, --  for Brutus is a real cool cat; So are they all, all cool cats, -- Come I to make this gig at Caesar's laying down.

--- applications/setCorrect.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-BROWSER="google-chrome-stable"
-# BROWSER="chrome"
-
-xdg-mime default ${BROWSER}.desktop x-scheme-handler/http
-xdg-mime default ${BROWSER}.desktop x-scheme-handler/https
-xdg-mime default ${BROWSER}.desktop text/html
-xdg-settings set default-web-browser ${BROWSER}.desktop
