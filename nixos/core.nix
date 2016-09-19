34d911889b7b4a02e2a8d05b26d54d9f804e91b8
Anders Sildnes <andsild@gmail.com>
2016-09-19 10:46:37 +0200

    Ad

--- nixos/core.nix
+++ nixos/core.nix
@@ -7,6 +7,7 @@
  boot.loader.grub.enable = true;
  boot.loader.grub.version = 2;
  boot.loader.grub.device = "/dev/sda";
+ boot.kernelPackages = pkgs.linuxPackages_latest;
 
  networking = {
    networkmanager.enable = true;
