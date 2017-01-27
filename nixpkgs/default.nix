f0c46ed3e6f3ca12bf862fe9edc5754b76b659bf
asildnes <andsild@gmail.com>
2017-01-27 20:15:33 +0100

    Although golf was originally restricted to wealthy, overweight Protestants, today it's open to anybody who owns hideous clothing. 		-- Dave Barry

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -1,3 +1,29 @@
-import ./pkgs {
-  pkgs = import <nixpkgs> {};
-}
+{ system ? builtins.currentSystem }:
+
+let 
+	pkgs = import <nixpkgs> { inherit system; };
+
+	callPackage = pkgs.lib.callPackageWith (pkgs // self);
+
+	self = {
+	  st = pkgs.callPackage ./pkgs/st {
+	    conf = import ./pkgs/st_config.nix {} ;
+	  };
+
+	  environment.variables = rec {
+	      VISUAL  = "nvim";
+	      EDITOR  = VISUAL;
+	      BROWSER = "chromium-browser";
+	    };
+
+	  neovim = pkgs.neovim.override {
+	    vimAlias = true;
+	    withPython = true;
+	    withPython3 = true;
+        withRuby = true;
+
+        withPyGUI = true;
+	    configure = import ./pkgs/nvim_config.nix { inherit pkgs; };
+	  };
+	};
+	in self
--- nixpkgs/pkgs/nvim_config.nix
+++ nixpkgs/pkgs/nvim_config.nix
@@ -1,13 +1,15 @@
 { pkgs }:
+# The virtualenv for python3 must have neovim, i.e `pip install neovim`
 {
     customRC = ''
         let g:python_host_prog=expand('~/python2env/bin/python')
-        let g:python3_host_prog=expand('~/defaultenv3/bin/python')
+        let g:python3_host_prog=expand('~/python3env/bin/python')
         exe 'source ' . expand('~/dotfiles/vim/init.vim')
         '';
     vam.pluginDictionaries = [
         {
             names = [
+            # This plugin parses nix configuration files in vim
                 "vim-nix"
             ];
         }
