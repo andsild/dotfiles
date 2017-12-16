f4a9aa47219d902e92f3adbdd8fd0357d38124ae
Anders Sildnes <anders.sildnes@cxense.com>
2017-12-16 15:31:14 +0100

    QOTD: 	"It's been Monday all week today."

---
+++ nixpkgs/pkgs/workrave/default.nix
@@ -0,0 +1,44 @@
+{ stdenv, fetchurl
+, autoconf, automake, gettext, intltool, libtool, pkgconfig
+, libICE, libSM, libXScrnSaver, libXtst, cheetah
+, glib, glibmm, gtk, gtkmm, atk, pango, pangomm, cairo, cairomm
+, dbus, dbus_glib, GConf, gconfmm, gdome2, gstreamer, libsigcxx }:
+
+stdenv.mkDerivation rec {
+  version = "1.10.6";
+  name = "workrave-${version}";
+
+  src = let
+    version_ = with stdenv.lib;
+      concatStringsSep "_" (splitString "." version);
+  in fetchurl {
+    name = "${name}.tar.gz";
+    url = "http://github.com/rcaelers/workrave/archive/v${version_}.tar.gz";
+    sha256 = "0q2p83n33chbqzdcdm7ykfsy73frfi6drxzm4qidxwzpzsxrysgq";
+  };
+
+  nativeBuildInputs = [
+    autoconf automake gettext intltool libtool pkgconfig
+  ];
+  buildInputs = [
+    libICE libSM libXScrnSaver libXtst cheetah
+    glib glibmm gtk gtkmm atk pango pangomm cairo cairomm
+    dbus dbus_glib GConf gconfmm gdome2 gstreamer libsigcxx
+  ];
+
+  preConfigure = "./autogen.sh";
+
+  meta = with stdenv.lib; {
+    description = "A program to help prevent Repetitive Strain Injury";
+    longDescription = ''
+      Workrave is a program that assists in the recovery and prevention of
+      Repetitive Strain Injury (RSI). The program frequently alerts you to
+      take micro-pauses, rest breaks and restricts you to your daily limit.
+    '';
+    homepage = http://www.workrave.org/;
+    downloadPage = https://github.com/rcaelers/workrave/releases;
+    license = licenses.gpl3;
+    maintainers = with maintainers; [ prikhi ];
+    platforms = platforms.linux;
+  };
+}
