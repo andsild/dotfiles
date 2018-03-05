c848b059056df6f9a6fe0eb14daee171d1b3bde1
Anders Sildnes <andsild@posteo.net>
2018-03-05 11:51:55 +0100

    You mentioned your name as if I should recognize it, but beyond the obvious facts that you are a bachelor, a solicitor, a freemason, and an asthmatic, I know nothing whatever about you. 		-- Sherlock Holmes, "The Norwood Builder"

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -14,17 +14,5 @@ let
 
     checkstyle = pkgs.callPackage ./pkgs/checkstyle {
     };
-
-	  st = pkgs.callPackage ./pkgs/st {
-	    conf = import ./pkgs/st_config.nix {} ;
-	  };
-
-	  workrave = pkgs.callPackage ./pkgs/workrave {
-      inherit (pkgs.gnome2) GConf gconfmm;
-      inherit (pkgs.python27Packages) cheetah;
-      gtk = pkgs.gtk2;
-      gtkmm = pkgs.gtkmm2;
-	  };
-
 	};
 	in self
