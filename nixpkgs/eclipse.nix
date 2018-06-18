fac244963cc89663dd18eef0c16934502a19c6af
Anders Sildnes <andsild@posteo.net>
2018-06-18 09:12:26 +0200

    You dialed 5483.

---
+++ nixpkgs/eclipse.nix
@@ -0,0 +1,9 @@
+self: super:
+
+{
+  # eclipses.myEclipse = (super.eclipses.eclipseWithPlugins {
+  #   eclipse = super.eclipses.eclipse-platform-47;
+  #   jvmArgs = [ "-Xmx2048m" "-Xms2048m" ];
+  #   plugins = with super.eclipses.plugins; [ checkstyle color-theme findbugs jdt vrapper testng ];
+  # });
+}
