69c3b2f0f0c2cd2054101fb59259b5416d336302
Anders Sildnes <andsild@posteo.net>
2020-04-22 20:36:35 +0200

    Sho' they got to have it against the law.  Shoot, ever'body git high, they wouldn't be nobody git up and feed the chickens.  Hee-hee. 		-- Terry Southern

--- nixpkgs/eclipse.nix
+++
@@ -1,9 +0,0 @@
-self: super:
-
-{
-  eclipse-platform = super.eclipses.eclipseWithPlugins {
-    eclipse = super.eclipses.eclipse-platform;
-    jvmArgs = [ "-Xmx2048m" "-Xms2048m" ];
-    plugins = with super.eclipses.plugins; [ checkstyle color-theme findbugs jdt vrapper testng ];
-  };
-}
